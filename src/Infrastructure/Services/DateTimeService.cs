eb30dc8e29f639c33790f763329e51a7e8e0c9de
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-11-03 21:02:14 +0100

    Add datetime service

---
+++ src/Infrastructure/Services/DateTimeService.cs
@@ -0,0 +1,9 @@
+﻿using Application.Common.Interfaces;
+
+namespace Infrastructure.Services
+{
+    internal sealed class DateTimeService : IDateTime
+    {
+        public DateTime Now => DateTime.Now;
+    }
+}
