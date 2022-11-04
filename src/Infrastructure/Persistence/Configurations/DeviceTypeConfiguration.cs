8401e25d5784bbea84cad247cd38d54a8d709ed9
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-11-04 16:02:08 +0100

    Add device type configuration

---
+++ src/Infrastructure/Persistence/Configurations/DeviceTypeConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    internal sealed class DeviceTypeConfiguration : IEntityTypeConfiguration<DeviceType>
+    {
+        public void Configure(EntityTypeBuilder<DeviceType> builder)
+        {
+            builder.Property(x => x.Name).HasMaxLength(255);
+
+            builder.HasMany(e => e.Children)
+                   .WithOne(e => e.Parent)
+                   .HasForeignKey(e => e.ParentId)
+                   .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
