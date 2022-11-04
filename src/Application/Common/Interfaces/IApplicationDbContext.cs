f1f64795d4e8a0236345911fdb85c307c6ad2266
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-11-04 16:00:52 +0100

    Add db context

---
+++ src/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Device> Devices { get; }
+        DbSet<DeviceType> DeviceTypes { get; }
+        DbSet<DeviceTypeProperty> DeviceTypeProperties { get; }
+        DbSet<DevicePropertyValue> DevicePropertyValues { get; }
+    }
+}
---
+++ src/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using Infrastructure.Persistence.Interceptors;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Infrastructure.Persistence
+{
+    public sealed class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        private readonly AuditableEntitySaveChangesInterceptor _auditableEntitySaveChangesInterceptor;
+
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options,
+            AuditableEntitySaveChangesInterceptor auditableEntitySaveChangesInterceptor) : base(options)
+        {
+            _auditableEntitySaveChangesInterceptor = auditableEntitySaveChangesInterceptor;
+        }
+
+        public DbSet<Device> Devices => Set<Device>();
+        public DbSet<DevicePropertyValue> DevicePropertyValues => Set<DevicePropertyValue>();
+        public DbSet<DeviceType> DeviceTypes => Set<DeviceType>();
+        public DbSet<DeviceTypeProperty> DeviceTypeProperties => Set<DeviceTypeProperty>();
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(builder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.AddInterceptors(_auditableEntitySaveChangesInterceptor);
+        }
+    }
+}
