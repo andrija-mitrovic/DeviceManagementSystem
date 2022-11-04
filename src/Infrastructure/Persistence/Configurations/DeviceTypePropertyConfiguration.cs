87a1334ad9d6a1da17434ba792f3ed79cc7a669a
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-11-04 16:02:56 +0100

    Add device type property entity configuration

---
+++ src/Infrastructure/Persistence/Configurations/DeviceTypePropertyConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    internal sealed class DeviceTypePropertyConfiguration : IEntityTypeConfiguration<DeviceTypeProperty>
+    {
+        public void Configure(EntityTypeBuilder<DeviceTypeProperty> builder)
+        {
+            builder.Property(x => x.Name).HasMaxLength(255);
+        }
+    }
+}
