bec992773c832708bfd3d4c571e45e18b57bb24a
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-11-04 16:01:56 +0100

    Add device entity configuration

---
+++ src/Infrastructure/Persistence/Configurations/DeviceConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    internal sealed class DeviceConfiguration : IEntityTypeConfiguration<Device>
+    {
+        public void Configure(EntityTypeBuilder<Device> builder)
+        {
+            builder.Property(x => x.Name).HasMaxLength(255);
+
+            builder.HasOne(c => c.DeviceType)
+                   .WithMany(c => c.Devices)
+                   .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
