eaf6ef7a5977b003b6185f779b279487fd842df4
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-11-04 16:02:25 +0100

    Add device property value configuration

---
+++ src/Infrastructure/Persistence/Configurations/DevicePropertyValueConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    internal sealed class DevicePropertyValueConfiguration : IEntityTypeConfiguration<DevicePropertyValue>
+    {
+        public void Configure(EntityTypeBuilder<DevicePropertyValue> builder)
+        {
+            builder.HasOne(e => e.Device)
+                   .WithMany(e => e.DevicePropertyValues)
+                   .HasForeignKey(e => e.DeviceId)
+                   .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
