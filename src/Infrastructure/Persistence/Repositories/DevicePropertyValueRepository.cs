b150b44986959ee7926c48e6e1441adfcf8095c8
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-11-05 09:18:56 +0100

    Add DevicePropertyValueRepository

---
+++ src/Infrastructure/Persistence/Repositories/DevicePropertyValueRepository.cs
@@ -0,0 +1,10 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    internal sealed class DevicePropertyValueRepository : GenericRepository<DevicePropertyValue>, IDevicePropertyValueRepository
+    {
+        public DevicePropertyValueRepository(ApplicationDbContext dbContext) : base(dbContext) { }
+    }
+}
