8dc76b418f5e4b225ba61a040ffc6fcafdba9546
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-11-05 09:19:06 +0100

    Add device repository

---
+++ src/Infrastructure/Persistence/Repositories/DeviceRepository.cs
@@ -0,0 +1,10 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    internal sealed class DeviceRepository : GenericRepository<Device>, IDeviceRepository
+    {
+        public DeviceRepository(ApplicationDbContext dbContext) : base(dbContext) { }
+    }
+}
