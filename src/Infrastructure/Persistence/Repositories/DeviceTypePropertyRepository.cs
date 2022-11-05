39f538c797ddffa05ff834c8197d6c82f9e98ccf
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-11-05 09:19:59 +0100

    Add DeviceTypeProperty Repository

---
+++ src/Infrastructure/Persistence/Repositories/DeviceTypePropertyRepository.cs
@@ -0,0 +1,10 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    internal sealed class DeviceTypePropertyRepository : GenericRepository<DeviceTypeProperty>, IDeviceTypePropertyRepository
+    {
+        public DeviceTypePropertyRepository(ApplicationDbContext dbContext) : base(dbContext) { }
+    }
+}
