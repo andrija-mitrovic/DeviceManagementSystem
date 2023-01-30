1ccfb614fe79663850c9c8ae0b829c0c30a8ded2
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2023-01-30 14:43:37 +0100

    Add DeviceTypeWithChildEntityBadRequestException

---
+++ src/Application/Common/Exceptions/DeviceTypeWithChildEntityBadRequestException.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+
+namespace Application.Common.Exceptions
+{
+    public sealed class DeviceTypeWithChildEntityBadRequestException : BadRequestException
+    {
+        public DeviceTypeWithChildEntityBadRequestException(int id)
+            : base($"{nameof(DeviceType)} with id: {id} has child entity so it cannot be deleted.")
+        {
+        }
+    }
+}
