48d9fa618beb3af19bde6f4e650cfc54aec2bce9
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2023-02-01 09:16:35 +0100

    Add custom exception

---
+++ src/Application/Common/Exceptions/DeviceTypeWithExistingDeviceBadRequestException.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+
+namespace Application.Common.Exceptions
+{
+    public sealed class DeviceTypeWithExistingDeviceBadRequestException : BadRequestException
+    {
+        public DeviceTypeWithExistingDeviceBadRequestException():
+            base($"There is at least one {nameof(Device)} with this {nameof(DeviceType)} in the database.")
+        { }
+    }
+}
