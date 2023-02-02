3acb09e32fe8b4f2411ae88559385bb6b8a0fd35
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2023-02-02 09:12:03 +0100

    Add device not found exception

---
+++ src/Application/Common/Exceptions/DeviceNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+
+namespace Application.Common.Exceptions
+{
+    public sealed class DeviceNotFoundException : NotFoundException
+    {
+        public DeviceNotFoundException(int id) : base($"{nameof(Device)} with id: {id} doesn't exist in the database.") { }
+    }
+}
