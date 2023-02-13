42221d32afd6b0ff3cb96ff91d25202c60594e56
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2023-02-13 08:42:04 +0100

    Add device query handler

---
+++ src/Application/Devices/Queries/GetDevices/GetDevicesQuery.cs
@@ -0,0 +1,9 @@
+﻿using Application.Common.DTOs;
+using MediatR;
+
+namespace Application.Devices.Queries.GetDevices
+{
+    public sealed class GetDevicesQuery : IRequest<List<DeviceDTO>>
+    {
+    }
+}
---
+++ src/Application/Devices/Queries/GetDevices/GetDevicesQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using Application.Common.DTOs;
+using Application.Common.Interfaces;
+using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Devices.Queries.GetDevices
+{
+    internal sealed class GetDevicesQueryHandler : IRequestHandler<GetDevicesQuery, List<DeviceDTO>>
+    {
+        private readonly IDeviceRepository _deviceRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<GetDevicesQueryHandler> _logger;
+
+        public GetDevicesQueryHandler(
+            IDeviceRepository deviceRepository, 
+            IMapper mapper, 
+            ILogger<GetDevicesQueryHandler> logger)
+        {
+            _deviceRepository = deviceRepository;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<List<DeviceDTO>> Handle(GetDevicesQuery request, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Retrieving devices...");
+
+            var devices = await _deviceRepository.GetAllAsync(true, cancellationToken);
+
+            return _mapper.Map<List<DeviceDTO>>(devices);
+        }
+    }
+}
