0e88ba5dae5b0da3928995a1b297029d8f547acf
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2023-02-14 13:32:04 +0100

    Add delete device command handler

---
+++ src/Application/Devices/Commands/DeleteDevice/DeleteDeviceCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.Devices.Commands.DeleteDevice
+{
+    public sealed class DeleteDeviceCommand : IRequest<Unit>
+    {
+        public int Id { get; init; }
+    }
+}
---
+++ src/Application/Devices/Commands/DeleteDevice/DeleteDeviceCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Devices.Commands.DeleteDevice
+{
+    internal sealed class DeleteDeviceCommandHandler : IRequestHandler<DeleteDeviceCommand, Unit>
+    {
+        private readonly IDeviceRepository _deviceRepository;
+        private readonly IDevicePropertyValueRepository _devicePropertyValueRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<DeleteDeviceCommandHandler> _logger;
+
+        public DeleteDeviceCommandHandler(
+            IDeviceRepository deviceRepository, 
+            IDevicePropertyValueRepository devicePropertyValueRepository,
+            IUnitOfWork unitOfWork, 
+            ILogger<DeleteDeviceCommandHandler> logger)
+        {
+            _deviceRepository = deviceRepository;
+            _devicePropertyValueRepository = devicePropertyValueRepository;
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+        }
+
+        public async Task<Unit> Handle(DeleteDeviceCommand request, CancellationToken cancellationToken)
+        {
+            var device = await _deviceRepository.GetDeviceWithDevicePropertyValueById(request.Id, false, cancellationToken);
+
+            if (device is null)
+            {
+                _logger.LogError(nameof(Device) + " with id: {DeviceId} was not found.", request.Id);
+                throw new DeviceNotFoundException(request.Id);
+            }
+
+            _deviceRepository.Delete(device);
+            _devicePropertyValueRepository.DeleteRange(device.DevicePropertyValues!);
+
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            _logger.LogInformation(nameof(Device) + " with id: {DeviceId} is successfully deleted.", request.Id);
+
+            return Unit.Value;
+        }
+    }
+}
