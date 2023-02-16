ea6a2f41c102d1efb879f58a8e1ee73bbead5f3f
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2023-02-16 17:14:33 +0100

    Add pagination header

---
+++ src/API/Extensions/HttpExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Application.Common.Helpers;
+using System.Text.Json;
+
+namespace Microsoft.AspNetCore.Http
+{
+    public static class HttpExtensions
+    {
+        public static void AddPaginationHeader(this HttpResponse response, MetaData metaData)
+        {
+            var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+            response.Headers.Add("Pagination", JsonSerializer.Serialize(metaData, options));
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+        }
+    }
+}
