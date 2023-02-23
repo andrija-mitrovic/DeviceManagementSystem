25cbfc4249700df1bd76a30549f51bd0594e446c
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2023-02-23 08:33:38 +0100

    Add test for helper function

---
+++ tests/Application.UnitTests/Common/Helpers/HelperFunctionTest.cs
@@ -0,0 +1,17 @@
+﻿using Application.Common.Helpers;
+using FluentAssertions;
+
+namespace Application.UnitTests.Common.Helpers
+{
+    public sealed class HelperFunctionTest
+    {
+        [Theory]
+        [InlineData("DeviceCreate")]
+        public void GetMethodName_ShouldGetMethodName_WhenArgumentIsPassed(string input)
+        {
+            var result = HelperFunction.GetMethodName(input);
+
+            result.Should().BeEquivalentTo(input);
+        }
+    }
+}
